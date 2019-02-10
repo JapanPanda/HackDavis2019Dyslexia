8fd9137c705674835a1b4a30c239bc70010084a4
Derick Phan <dtph@ucdavis.edu>
2019-02-10 12:15:15 -0800

    update

--- WebService/public/js/audio-button.js
+++ WebService/public/js/audio-button.js
@@ -1,18 +1,23 @@
 var current_play = -1;
 var isRunning = false;
 var audio = document.getElementsByTagName('audio');
+var sentences = $(".short-story span");
+var stop = false
 
-async function play_audio() {
-var current_play = 0
-var isRunning = false
-
+async function play_audio(audio) {
+stop = false;
+  $('#play-audio').removeClass('button-success');
+  $('#play-audio').addClass('button-error');
+  $('#play-audio').disabled = true;
   if (!isRunning) {
-  var sentences = $(".short-story span");
     isRunning = true
     for (var i = 0; i < audio.length; i++) {
       current_play = i
-      play_audio_delay(i, sentences);
+      play_audio_delay(i, audio, sentences);
       await sleep(audio[i].duration * 1000);
+      if (stop == true) {
+        break;
+      } 
       console.log('finished');
     }
     isRunning = false
@@ -20,6 +25,7 @@ var isRunning = false
 }
 
 async function play_curr(audio) {
+  stop = false
   if (!isRunning) {
     if (current_play < 0) {
       current_play = 0;
@@ -34,6 +40,7 @@ async function play_curr(audio) {
 }
 
 async function play_prev(audio) {
+  stop = false
   if (!isRunning) {
     current_play -= 1;
     if (current_play < 0) {
@@ -48,12 +55,12 @@ async function play_prev(audio) {
   }
 }
 
-async function play_one() {
+async function play_one(audio) {
   if (!isRunning) {
     current_play += 1;
     isRunning = true;
     var sentences = $(".short-story span");
-    play_audio_delay(current_play, sentences);
+    play_audio_delay(current_play, audio, sentences);
     await sleep(audio[current_play].duration * 1000);
 
     // Set next audio to play
@@ -69,7 +76,7 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-async function play_audio_delay(i, sentences) {
+async function play_audio_delay(i, audio, sentences) {
     audio[i].play();
     sentences[i].style.backgroundColor = "yellow";
     audio[i].onended = async function() {
@@ -78,10 +85,14 @@ async function play_audio_delay(i, sentences) {
     }
 }
 
-function pause_audio() {
-  audio[current_play].pause();
+async function pause_audio() {
+  audio[current_play+1].pause()
+  stop = true
+  console.log(current_play);
+  $('#play-audio').removeClass('button-error');
+  $('#play-audio').addClass('button-success');
+  $('#play-audio').disabled = false;
 }
-
   /*audio[0].play();
   sentences[0].style.backgroundColor = "yellow";
   audio[0].onended = function() {
